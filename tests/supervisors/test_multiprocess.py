7d1a0055aee5e281accc646b559ddd147486bf8a
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-09-12 10:51:05 +0200

    Remove race from multiprocess health check test (#3128)

--- tests/supervisors/test_multiprocess.py
+++ tests/supervisors/test_multiprocess.py
@@ -92,17 +92,18 @@ def test_multiprocess_health_check() -> None:
     """
     config = Config(app=app, workers=2)
     supervisor = Multiprocess(config, sockets=[])
-    threading.Thread(target=supervisor.run, daemon=True).start()
-    time.sleep(1)
-    process = supervisor.processes[0]
-    process.kill()
-    assert not process.is_alive()
-    deadline = time.monotonic() + 10
-    while not all(p.is_alive() for p in supervisor.processes):  # pragma: no cover
-        assert time.monotonic() < deadline, "Timed out waiting for processes to be alive"
-        time.sleep(0.1)
-    supervisor.signal_queue.append(signal.SIGINT)
-    supervisor.join_all()
+    try:
+        supervisor.init_processes()
+        process = supervisor.processes[0]
+        process.kill()
+        process.join()
+        supervisor.keep_subprocess_alive()
+
+        assert process not in supervisor.processes
+        assert all(process.is_alive() for process in supervisor.processes)
+    finally:
+        supervisor.terminate_all()
+        supervisor.join_all()
 
 
 @new_console_in_windows
